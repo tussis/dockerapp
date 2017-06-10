0ab0f203dd12d676ed407a6eef1161528b1993e1
Chris Milam <chris.milam@gmail.com>
2017-06-10 12:30:02 -0700

    more express setup

--- app.js
+++ app.js
@@ -4,12 +4,17 @@
 
 
 var express = require('express');
+var path = require('path');
 var routes = require('./routes/');
 
 var app = express();
 
 app.set('views', 'views');
 app.set('view engine', 'jade');
+
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 //app.get('/', function(req, res) {
 //    res.render('home', {
